5493356c8e6fa9faf738940d01cafb689d7a1489
YasinArafat <117991006+YasinArafat1@users.noreply.github.com>
2023-09-06 21:16:31 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program:A
+    {
+        public void Multiplication() 
+        {
+            c = a * b;
+            Console.WriteLine("Mulitplication is : " + c);
+
+        }
+        public void Division()
+        {
+            c = a / b;
+            Console.WriteLine("Division is "+c);
+        }
+        public void Modulus()
+        {
+            c = a % b;
+            Console.WriteLine("Moudlus is "+c);
+
+        }
+        static void Main(string[] args)
+        {
+
+            A a = new A();
+            a.Addition();
+            a.Sub();
+            Program p = new Program();
+            p.Multiplication();
+            p.Division();
+            p.Modulus();
+
+        Console.ReadKey();
+        }
+    }
+
+    public class A 
+    {
+        protected int a = 200;
+        protected int b = 100;
+        protected int c;
+
+        public void Addition() 
+        {
+            c = a + b;
+            Console.WriteLine("Addition: "+c);
+        }
+        public void Sub()
+        {
+            c = a - b;
+            Console.WriteLine("Subtraction is : " + c);
+        }
+
+
+
+    }
+
+   
+}
